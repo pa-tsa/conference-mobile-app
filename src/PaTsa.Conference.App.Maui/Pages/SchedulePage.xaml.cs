bd6bbcff8d3364dc1ca793fe59ac93d251b3021f
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-04-11 20:08:40 -0400

    Wired events for interacting with schedule filters

--- src/PaTsa.Conference.App.Maui/Pages/SchedulePage.xaml.cs
+++ src/PaTsa.Conference.App.Maui/Pages/SchedulePage.xaml.cs
@@ -1,13 +1,36 @@
+using System;
 using Microsoft.Maui.Controls;
 using PaTsa.Conference.App.Maui.ViewModels;
 
 namespace PaTsa.Conference.App.Maui.Pages;
 
-public partial class SchedulePage : ContentPage
+public partial class SchedulePage
 {
     public SchedulePage(ConferenceEventGroupViewModel conferenceEventGroupViewModel)
     {
         InitializeComponent();
         BindingContext = conferenceEventGroupViewModel;
+    }
+
+    private void AllEventsButton_OnClicked(object sender, EventArgs e)
+    {
+        AllEventsButton.Style = Application.Current?.Resources["RedButtonFilled"] as Style;
+        FavoriteEventsButton.Style = Application.Current?.Resources["RedButtonOutline"] as Style;
+
+        EventsFilterButton.IsEnabled = true;
+    }
+
+    private void FavoriteEventsButton_OnClicked(object sender, EventArgs e)
+    {
+        AllEventsButton.Style = Application.Current?.Resources["RedButtonOutline"] as Style;
+        FavoriteEventsButton.Style = Application.Current?.Resources["RedButtonFilled"] as Style;
+
+        EventsFilterButton.IsEnabled = false;
+        EventsFilterVerticalStackLayout.IsVisible = false;
+    }
+
+    private void EventsFilterButton_OnClicked(object sender, EventArgs e)
+    {
+        EventsFilterVerticalStackLayout.IsVisible = !EventsFilterVerticalStackLayout.IsVisible;
     }
 }
