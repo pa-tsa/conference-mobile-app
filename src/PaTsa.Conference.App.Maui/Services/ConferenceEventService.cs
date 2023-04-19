93cd64a2777e80dc25869ea61e3a08c5c1dc3a40
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2023-04-18 20:29:22 -0400

    Added ability to filter by EventId to the service

--- src/PaTsa.Conference.App.Maui/Services/ConferenceEventService.cs
+++ src/PaTsa.Conference.App.Maui/Services/ConferenceEventService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 using PaTsa.Conference.App.Maui.Models;
 
@@ -8,6 +9,8 @@ namespace PaTsa.Conference.App.Maui.Services;
 
 public class ConferenceEventService
 {
+    public const int PageSize = 30;
+
     private readonly HttpClient _httpClient;
 
     public ConferenceEventService()
@@ -15,18 +18,48 @@ public class ConferenceEventService
         _httpClient = new HttpClient();
     }
 
-    private static HttpRequestMessage ConstructGetConferenceEventsRequest(string typesFilter, int pageNumber)
+    private static HttpRequestMessage ConstructGetConferenceEventsRequest(
+        string eventIdsFilter,
+        string typesFilter,
+        int pageNumber)
     {
+        var queryStringBuilder = new StringBuilder();
+
+        if (!string.IsNullOrWhiteSpace(eventIdsFilter))
+        {
+            queryStringBuilder.Append(queryStringBuilder.Length == 0 ? "?" : "&");
+            queryStringBuilder.Append($"eventIds={eventIdsFilter}");
+        }
+
+        if (!string.IsNullOrWhiteSpace(typesFilter))
+        {
+            queryStringBuilder.Append(queryStringBuilder.Length == 0 ? "?" : "&");
+            queryStringBuilder.Append($"types={typesFilter}");
+        }
+
+        queryStringBuilder.Append(queryStringBuilder.Length == 0 ? "?" : "&");
+        queryStringBuilder.Append($"pageNumber={pageNumber}");
+
+        queryStringBuilder.Append(queryStringBuilder.Length == 0 ? "?" : "&");
+        queryStringBuilder.Append($"pageSize={PageSize}");
+
         var httpRequestMessage = new HttpRequestMessage(
             HttpMethod.Get,
-            $"https://pa-tsa-conference-app-api-app.azurewebsites.net/api/ConferenceEvents?types={typesFilter}&pageNumber={pageNumber}&pageSize=20");
+            $"https://pa-tsa-conference-app-api-app.azurewebsites.net/api/ConferenceEvents{queryStringBuilder}");
 
         return httpRequestMessage;
     }
 
-    public async Task<List<ConferenceEventModel>> GetConferenceEvents(string typesFilter, int pageNumber)
+    public async Task<List<ConferenceEventModel>> GetConferenceEvents(
+        IEnumerable<string> eventIds,
+        IEnumerable<string> types,
+        int pageNumber)
     {
-        var httpResponseMessage = await _httpClient.SendAsync(ConstructGetConferenceEventsRequest(typesFilter, pageNumber));
+        var httpResponseMessage = await _httpClient.SendAsync(
+            ConstructGetConferenceEventsRequest(
+                string.Join(',', eventIds),
+                string.Join(',', types),
+                pageNumber));
 
         if (!httpResponseMessage.IsSuccessStatusCode) return new List<ConferenceEventModel>();
 
