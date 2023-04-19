f95d33f0598703b41fda63518e0a16eb0e1f4e64
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2023-04-18 20:30:02 -0400

    Updated page to work with favorite events

--- src/PaTsa.Conference.App.Maui/Pages/SchedulePage.xaml.cs
+++ src/PaTsa.Conference.App.Maui/Pages/SchedulePage.xaml.cs
@@ -6,32 +6,38 @@ namespace PaTsa.Conference.App.Maui.Pages;
 
 public partial class SchedulePage
 {
+    protected ScheduleViewModel ScheduleViewModel => BindingContext as ScheduleViewModel;
+
     public SchedulePage(ScheduleViewModel scheduleViewModel)
     {
         InitializeComponent();
         BindingContext = scheduleViewModel;
     }
 
-    private void AllEventsButton_OnClicked(object sender, EventArgs e)
+    private async void AllEventsButton_OnClicked(object sender, EventArgs e)
     {
         AllEventsButton.Style = Application.Current?.Resources["RedButtonFilled"] as Style;
         FavoriteEventsButton.Style = Application.Current?.Resources["RedButtonOutline"] as Style;
 
         EventsFilterButton.IsEnabled = true;
+
+        await ScheduleViewModel.ShowAllConferenceEventsCommand.ExecuteAsync(null);
     }
 
-    private void FavoriteEventsButton_OnClicked(object sender, EventArgs e)
+    private void EventsFilterButton_OnClicked(object sender, EventArgs e)
+    {
+        ShowHideEventsFilter();
+    }
+
+    private async void FavoriteEventsButton_OnClicked(object sender, EventArgs e)
     {
         AllEventsButton.Style = Application.Current?.Resources["RedButtonOutline"] as Style;
         FavoriteEventsButton.Style = Application.Current?.Resources["RedButtonFilled"] as Style;
 
         EventsFilterButton.IsEnabled = false;
         EventsFilterVerticalStackLayout.IsVisible = false;
-    }
 
-    private void EventsFilterButton_OnClicked(object sender, EventArgs e)
-    {
-        ShowHideEventsFilter();
+        await ScheduleViewModel.ShowFavoriteConferenceEventsCommand.ExecuteAsync(null);
     }
 
     private void FilterEventsButton_OnClicked(object sender, EventArgs e)
@@ -42,5 +48,15 @@ public partial class SchedulePage
     private void ShowHideEventsFilter()
     {
         EventsFilterVerticalStackLayout.IsVisible = !EventsFilterVerticalStackLayout.IsVisible;
+    }
+
+    private void FavoriteImageButton_OnClicked(object sender, EventArgs e)
+    {
+        if (ScheduleViewModel.ShowFavoritesOnly) return;
+
+        ((ImageButton)sender).Source = new FileImageSource
+        {
+            File = "heart_fill.png"
+        };
     }
 }
